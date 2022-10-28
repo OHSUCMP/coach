d0fe61a225c087714ae77244fbd52057904f5dcb
Amy Yates <yateam@ohsu.edu>
2022-10-28 14:06:39 -0700

    Modify average BP calculation
    - Average BP calculation matches CQL, using the BP Set average
    - 2 month chart button has been replaced with "Most Recent" to show the BP Set when possible
    - Chart falls back on 2 months if no set exists

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -1,3 +1,11 @@
+// Match the enum ObservationSource in Java
+const BPSource = Object.freeze({
+	Home: "HOME",
+	HomeBluetooth: "HOME_BLUETOOTH",
+	Office: "OFFICE",
+	Unknown: "UNKNOWN"
+})
+
 function doClearSupplementalData(_callback) {
     $.ajax({
         method: "POST",
@@ -82,32 +90,92 @@ function populateAdverseEvents() {
     }
 }
 
+// Sort blood pressures in date order
+function sortByDateAsc(bps) {
+	return bps.sort((a, b) => Number(a.readingDate) - Number(b.readingDate));
+}
+
+// Sort blood pressures in reverse date order
+function sortByDateDesc(bps) {
+	return bps.sort((a, b) => Number(b.readingDate) - Number(a.readingDate));
+}
+
+/**
+ * Get the set of BPs to use in average calculation or null if a set doesn't exist
+ * @param {*} bps
+ * @returns
+ */
+function getBPSet(bps) {
+    let bpsDesc = sortByDateDesc(bps);
+    let set = bpsDesc.reduce((acc, bp) => {
+        if (acc.score >= 4.0) {
+            return acc;
+        }
+        acc.bpset.push(bp);
+        if (bp.source === BPSource.Home | bp.source === BPSource.HomeBluetooth) {
+            acc.score += 0.667;
+        } else {
+            // Anything explicitly not home is considered OFFICE and given 1 point
+            acc.score += 1.0;
+        } 
+        return acc;
+    }, {
+        score: 0.0,
+        bpset: []
+    });
+
+    if (set.score >= 4) {
+        return set.bpset;
+    }
+
+    return null;
+}
+
+function getBPSetStartDate(bps) {
+    const bpset = getBPSet(bps);
+    if (bpset) {
+        const sorted = sortByDateAsc(bpset);
+        return new Date(sorted[0].readingDate)
+    } 
+
+    // Fall back to 2 months if no set
+    let startDate = new Date();
+    startDate.setMonth(startDate.getMonth() - 2);
+    return startDate;
+}
+    
+/* Calculate the BP Average using the same logic from the recommendations */
+function calculateAverageBP(bps) {
+    const bpset = getBPSet(bps);
+    if (bpset) {
+        const avgSys = bpset.reduce((acc,bp) => acc + bp.systolic.value, 0)/bpset.length;
+        const avgDia = bpset.reduce((acc,bp) => acc + bp.diastolic.value, 0)/bpset.length;
+        return {
+            systolic: avgSys,
+            diastolic: avgDia
+        };
+
+    }
+
+    return null;
+}
+      
 function populateSummaryDiv() {
-    let data = window.bpchart.data;
     let totalSystolic = 0;
     let totalDiastolic = 0;
     let avgSystolic = 0;
     let avgDiastolic = 0;
 
-    let hasData = Array.isArray(data) && data.length > 0;
+    let hasData = Array.isArray(window.bpdata) && window.bpdata.length > 0;
 
     if (hasData) {
-        let systolicCount = 0;
-        let diastolicCount = 0;
+        let avg = calculateAverageBP(window.bpdata);
 
-        data.forEach(function (o) {
-            if (o.systolic) {
-                totalSystolic += o.systolic.value;
-                systolicCount += 1;
-            }
-            if (o.diastolic) {
-                totalDiastolic += o.diastolic.value;
-                diastolicCount += 1;
-            }
-        });
-
-        avgSystolic = Math.round(totalSystolic / systolicCount);
-        avgDiastolic = Math.round(totalDiastolic / diastolicCount);
+        // TODO: Might be better to hide average entirely when there's no data?
+        if (avg) {
+            avgSystolic = Math.round(avg.systolic);
+            avgDiastolic = Math.round(avg.diastolic);
+        }
     }
     $('#avgSystolic').html(avgSystolic);
     $('#avgDiastolic').html(avgDiastolic);
@@ -133,7 +201,7 @@ function populateSummaryDiv() {
 
     let el = $('#bpIcon');
     $(el).html("<img src='/images/" + indicator + "-icon.png' class='icon' alt='" + indicator + "' />");
-    $('#bpLabel').html('Average BP:');
+    $('#bpLabel').html('Recent BP Average:').attr('title', 'Averages the last several readings to compare to your goal')
 
     let el2 = $('#bpGoalMetLabel');
     if (hasData) {
@@ -160,9 +228,9 @@ function buildPointStyleArray(data) {
     // see https://www.chartjs.org/docs/latest/configuration/elements.html for options
     let arr = [];
     data.forEach(function(item) {
-        if (item.source === 'HOME' || item.source === 'HOME_BLUETOOTH') {
+        if (item.source === BPSource.Home || item.source === BPSource.HomeBluetooth) {
             arr.push('circle');
-        } else if (item.source === 'OFFICE') {
+        } else if (item.source === BPSource.Office) {
             arr.push('rect');
         }
     });
