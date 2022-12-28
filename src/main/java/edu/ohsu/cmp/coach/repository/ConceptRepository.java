a3b0fb600c43e303a5bfbe3121954687ad7a0bf5
Matt Storer <storer@ohsu.edu>
2022-12-28 10:01:08 -0800

    replaced antihypertensive valueset 2.16.840.1.113762.1.4.1178.72 with 2.16.840.1.113762.1.4.1200.242 for ticket #21 and fixed associated JPA persistence bug related to creating a new ValueSet that references existing Concepts.

--- src/main/java/edu/ohsu/cmp/coach/repository/ConceptRepository.java
+++ src/main/java/edu/ohsu/cmp/coach/repository/ConceptRepository.java
@@ -2,7 +2,10 @@ package edu.ohsu.cmp.coach.repository;
 
 import edu.ohsu.cmp.coach.entity.Concept;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ConceptRepository extends JpaRepository<Concept, Long> {
-    Concept findOneByCodeSystemNameAndCode(String codeSystemName, String code);
+
+    @Query("select c from Concept c where c.code=:code and c.codeSystem=:codeSystem and c.codeSystemVersion=:version")
+    Concept findConcept(String code, String codeSystem, String version);
 }
--- src/main/java/edu/ohsu/cmp/coach/service/ValueSetService.java
+++ src/main/java/edu/ohsu/cmp/coach/service/ValueSetService.java
@@ -1,23 +1,26 @@
 package edu.ohsu.cmp.coach.service;
 
+import edu.ohsu.cmp.coach.entity.Concept;
 import edu.ohsu.cmp.coach.entity.ValueSet;
 import edu.ohsu.cmp.coach.repository.ValueSetRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @Service
+@Transactional
 public class ValueSetService extends AbstractService {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     private VSACService vsacService;
+
+    @Autowired
+    private ConceptService conceptService;
 
     @Autowired
     private ValueSetRepository repository;
@@ -31,11 +34,36 @@ public class ValueSetService extends AbstractService {
             ValueSet valueSet = getValueSet(oid);
             if (valueSet != null) {
                 logger.info("refreshing ValueSet with oid=" + oid);
+
+                // the ValueSet already exists in the local DB, so it will be prepopulated with Concepts that also
+                // exist in the local DB.  calling valueSet.update() with the fresh data from VSAC will apply those
+                // fresh chances to the persisted objects, adding any that don't exist and removing those that are
+                // missing
+
                 valueSet.update(vsacService.getValueSet(oid));
+
             } else {
                 logger.info("acquiring new ValueSet with oid=" + oid);
                 valueSet = vsacService.getValueSet(oid);
+
+                // the ValueSet does not yet exist in the local DB, so the object we're working with is straight
+                // from VSAC.  none of its Concepts come from the local DB and don't have IDs assigned.  however, these
+                // referenced Concepts *may* be represented in the local DB.  if they are, we *need* to replace those
+                // copies from VSAC with the locally persisted copies, so that JPA will perform an update instead of
+                // trying to recreate them with inserts.  this block of code below performs that operation
+
+                Set<Concept> concepts = new LinkedHashSet<>();
+                for (Concept c : valueSet.getConcepts()) {
+                    Concept existingConcept = conceptService.getConcept(c.getCode(), c.getCodeSystem(), c.getCodeSystemVersion());
+                    if (existingConcept != null) {
+                        concepts.add(existingConcept);
+                    } else {
+                        concepts.add(c);
+                    }
+                }
+                valueSet.setConcepts(concepts);
             }
+
             repository.save(valueSet);
 
         } catch (Exception e) {
