b936d040801e981e24b1dbd9fe8307982dddd44e
Matt Storer <storer@ohsu.edu>
2023-02-21 14:58:52 -0800

    fixed serialized names representation.  now successfully deserializes. (#25)

--- src/main/java/edu/ohsu/cmp/coach/model/omron/AccessTokenResponse.java
+++ src/main/java/edu/ohsu/cmp/coach/model/omron/AccessTokenResponse.java
@@ -1,10 +1,21 @@
 package edu.ohsu.cmp.coach.model.omron;
 
+import com.google.gson.annotations.SerializedName;
+
 public class AccessTokenResponse {
+    @SerializedName("id_token")
     private String idToken;         // id of the user who performed the authorization
+
+    @SerializedName("access_token")
     private String accessToken;
+
+    @SerializedName("expires_in")
     private Integer expiresIn;
+
+    @SerializedName("token_type")
     private String tokenType;
+
+    @SerializedName("refresh_token")
     private String refreshToken;
 
     public String getIdToken() {
