9c2e712739318ef475f616609b6d4e79277b38cf
Matthew Storer <storer@ohsu.edu>
2023-10-26 14:39:28 -0700

    added logic to permit certain tokens to be specified in a contact message for #74.  tokens must be wrapped in {curly braces}, and include the following :
    {readings} : the number of home BP readings collected within the last 30 days;
    {days} : the number of days over which the specified readings were collected;
    {systolic} : the average systolic BP for ALL readings (home, office, etc.) using the same logic as what's employed in the UI;
    {diastolic} : the average diastolic BP for ALL readings (home, office, etc.) using the same logic as what's employed in the UI.

---
+++ src/main/java/edu/ohsu/cmp/coach/model/BloodPressureSummaryModel.java
@@ -0,0 +1,98 @@
+package edu.ohsu.cmp.coach.model;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+public class BloodPressureSummaryModel {
+    private static final Integer MS_IN_DAY = 1000 * 60 * 60 * 24; // 86,400,000
+
+    private Integer avgSystolic;
+    private Integer avgDiastolic;
+    private Integer recentHomeBPReadingsCount;
+    private Integer recentHomeBPReadingsDayCount;
+
+    public BloodPressureSummaryModel(List<BloodPressureModel> list) {
+        List<BloodPressureModel> bpList = new ArrayList<>();     // create a copy of list to work with, don't want to modify the original
+        bpList.addAll(list);
+        bpList.sort((o1, o2) -> o1.getReadingDate().compareTo(o2.getReadingDate()) * -1); // sort newest first
+
+        long timeframeStartTS = getStartOfDay30DaysAgoTS();
+        Long earliestHomeReadingWithinTimeframeTS = null;
+        Long latestHomeReadingWithinTimeframeTS = null;
+        recentHomeBPReadingsCount = 0;
+        for (BloodPressureModel bpm : bpList) {
+            if (bpm.isHomeReading() && bpm.getReadingDateTimestamp() >= timeframeStartTS) {
+                recentHomeBPReadingsCount++;
+                if (earliestHomeReadingWithinTimeframeTS == null || bpm.getReadingDateTimestamp() < earliestHomeReadingWithinTimeframeTS) {
+                    earliestHomeReadingWithinTimeframeTS = bpm.getReadingDateTimestamp();
+                }
+                if (latestHomeReadingWithinTimeframeTS == null || bpm.getReadingDateTimestamp() > latestHomeReadingWithinTimeframeTS) {
+                    latestHomeReadingWithinTimeframeTS = bpm.getReadingDateTimestamp();
+                }
+            }
+        }
+        recentHomeBPReadingsDayCount = earliestHomeReadingWithinTimeframeTS != null && latestHomeReadingWithinTimeframeTS != null ?
+                Math.round((float) (latestHomeReadingWithinTimeframeTS - earliestHomeReadingWithinTimeframeTS) / MS_IN_DAY) :
+                30; // default to 30 days
+
+
+        // calculate average using same logic as in the UI
+        List<BloodPressureModel> bpSet = buildBPSet(bpList);
+        if (bpSet != null) {
+            int totalSystolic = 0;
+            int totalDiastolic = 0;
+
+            for (BloodPressureModel bpm : bpSet) {
+                totalSystolic += bpm.getSystolic().getValue();
+                totalDiastolic += bpm.getDiastolic().getValue();
+            }
+
+            avgSystolic = Math.round((float) totalSystolic / bpSet.size());
+            avgDiastolic = Math.round((float) totalDiastolic / bpSet.size());
+        }
+    }
+
+    public Integer getAvgSystolic() {
+        return avgSystolic;
+    }
+
+    public Integer getAvgDiastolic() {
+        return avgDiastolic;
+    }
+
+    public Integer getRecentHomeBPReadingsCount() {
+        return recentHomeBPReadingsCount;
+    }
+
+    public Integer getRecentHomeBPReadingsDayCount() {
+        return recentHomeBPReadingsDayCount;
+    }
+
+    private long getStartOfDay30DaysAgoTS() {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(new Date());
+        cal.add(Calendar.DATE, -30);
+        cal.set(Calendar.HOUR, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+        return cal.getTime().getTime();
+    }
+
+    private List<BloodPressureModel> buildBPSet(List<BloodPressureModel> bpList) {
+        List<BloodPressureModel> bpSet = new ArrayList<>();
+        double score = 0.0;
+        for (BloodPressureModel bp : bpList) {
+            if (score >= 4.0) break;
+            bpSet.add(bp);
+            if (bp.getSource() == ObservationSource.HOME || bp.getSource() == ObservationSource.COACH_UI || bp.getSource() == ObservationSource.OMRON) {
+                score += 0.334;
+            } else {
+                score += 1.0;
+            }
+        }
+        return score > 4.0 ? bpSet : null;
+    }
+}
