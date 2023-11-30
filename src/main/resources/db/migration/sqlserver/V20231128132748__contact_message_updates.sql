420c965e9a7e4c1fae456a80e6b8150557e3c363
Matt Storer <storer@ohsu.edu>
2023-11-30 12:42:40 -0800

    added <ADD MORE DETAIL HERE> tags to the end of relevant contact messages for #74

--- src/main/resources/db/migration/sqlserver/V20231128132748__contact_message_updates.sql
+++ src/main/resources/db/migration/sqlserver/V20231128132748__contact_message_updates.sql
@@ -3,7 +3,7 @@ alter table contact_message add belowText varchar(1000);
 go
 
 update contact_message
-set body='I collected {readings} BP readings over the last {days} days, and my blood pressure is still above goal; my BP average is {systolic}/{diastolic}.  I am currently taking {meds}.  What steps would you like me to take next?',
+set body='I collected {readings} BP readings over the last {days} days, and my blood pressure is still above goal; my BP average is {systolic}/{diastolic}.  I am currently taking {meds}.  What steps would you like me to take next?  <ADD MORE DETAILS HERE>',
     aboveText='<p>Add any of these details to the note below to help your doctor get a full picture of your health. Factors that can impact your BP include:</p>
 <ul>
 <li>Missed doses of medicines over the last week</li>
@@ -17,7 +17,7 @@ where messageKey='recommend-pharma'
    or messageKey='recommend-advancing-treatment';
 
 update contact_message
-set body='I experienced a side effect recently, I believe due to blood pressure management.  To safely manage my blood pressure at home, what steps would you like me to take next?',
+set body='I experienced a side effect recently, I believe due to blood pressure management.  To safely manage my blood pressure at home, what steps would you like me to take next?  <ADD MORE DETAILS HERE>',
     aboveText='<p>Add details regarding your side effect to the message below. These details can help your doctor get a full picture of your health. <a href="/side-effects" target="_blank">Click here</a> for a complete list of side effects. Relevant details could be:</p>
 <ul>
 <li>Details about side effect</li>
